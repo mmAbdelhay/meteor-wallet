650615760eacedda3baafbfee01437d633f5c97c
muhammad abdelhay <73421112+mmAbdelhay@users.noreply.github.com>
2022-01-24 21:28:38 +0200

    Update main.js

--- server/main.js
+++ server/main.js
@@ -3,7 +3,6 @@ import "../imports/api/ContactsCollection";
 import "../imports/api/ContactsMethods";
 import "../imports/api/ContactsPublications";
 import { Accounts } from "meteor/accounts-base";
-require("dotenv").config();
 
 Meteor.startup(() => {
   if (!Accounts.findUserByUsername(process.env.ADMIN_USERNAME)) {
